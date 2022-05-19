da0f9159f6bf8068ff348c7763a6c330e0d7c718
Claudio Lombao <claulom42@gmail.com>
2022-05-19 19:12:03 -0300

    Archivos

--- Desafio_Clase6/archivos.js
+++ Desafio_Clase6/archivos.js
@@ -1,10 +1,11 @@
 const fs = require('fs')
 
 
-const productos = [];
+
 class Archivo{
     constructor(nombre){
         this.nombre= nombre;
+        this.productos = [];
     }
 
     async leer (){
@@ -15,26 +16,49 @@ class Archivo{
             console.error([])
         }
     }
-    async guardar(producto){
+
+    getId(){
+        const l = this.productos.length
+        if(l < 1) return 0
+        return this.productos[this.productos.length - 1].id
+    }
+    
+   
+    async save(producto){
+        const id = this.getId()
+        this.productos.push({
+            ...producto, ...{id: id + 1}
+        })
         try{
-            productos.push(producto)
-            await fs.promises.writeFile(this.nombre, JSON.stringify(productos, null, '\t'))
+          await  fs.promises.writeFile(this.nombre, JSON.stringify(this.productos, null, '\t'))
         }catch(err){
             console.error("Can`t save file: " + err)
         }
     }
+    getById(id){
+        return this.productos.find(p => p.id == id)
+    }
+    getAll(){
+        return this.productos
+    }
+    deleteById(id){
+        const idx = this.productos.findIndex(p => p.id == id)
+        this.productos.splice(idx, 1)
+      
+    }
     async borrar(){
-        try{
-            await fs.promises.unlink(this.nombre, (err)=>{
-                if(err) return console.log('Error delete file: ' + err)
-            })
-        }catch(err){
-            console.error("Error delete file: " + err)
-        }
+        this.productos = []
+        await fs.promises.readFile(this.nombre, 'utf-8')
     }
 }
 const archivo = new Archivo("./file/productos.txt")
-archivo.guardar({title: "Remeras", price: 3499.50, thumbnail: "https://http2.mlstatic.com/D_NQ_NP_704108-MLA49333256921_032022-O.webp", id: productos.length + 1})
-archivo.guardar({title: "Zapatillas", price: 9500.50, thumbnail: "https://http2.mlstatic.com/D_NQ_NP_669795-MLA49152599709_022022-O.webp", id: productos.length + 1})
-archivo.guardar({title: "Celular", price: 90000.50, thumbnail: "https://http2.mlstatic.com/D_NQ_NP_995741-MLA47861036467_102021-O.webp", id: productos.length + 1})
+archivo.save({title: "Remeras", price: 3499.50, thumbnail: "https://http2.mlstatic.com/D_NQ_NP_704108-MLA49333256921_032022-O.webp"})
+archivo.save({title: "Zapatillas", price: 9500.50, thumbnail: "https://http2.mlstatic.com/D_NQ_NP_669795-MLA49152599709_022022-O.webp"})
+archivo.save({title: "Celular", price: 90000.50, thumbnail: "https://http2.mlstatic.com/D_NQ_NP_995741-MLA47861036467_102021-O.webp"})
+archivo.save({title: "Tablet", price: 29000.50, thumbnail: "https://http2.mlstatic.com/D_NQ_NP_995741-MLA47861036467_102021-O.webp"})
+
 archivo.leer()
+
+archivo.deleteById(2)
+
+archivo.leer
